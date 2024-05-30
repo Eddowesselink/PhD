b71679dc8c62e65e8e65eb44e134b254c52a8026
Eddo Wesselink <eddo_wesselink@msn.com>
2024-05-30 16:57:29 +0200

    changed reamde and CNN new push

--- README.md
+++ README.md
@@ -50,7 +50,7 @@ pip install -r requirements.txt
 ```
 This will install all the required packages listed in the `requirements.txt` file. Make sure you have `pip` installed and configured on your system.
 
-### Usage
+### Usage 
 
 To use the code, follow these steps:
 
@@ -59,14 +59,22 @@ To use the code, follow these steps:
 ```bash
 git clone https://github.com/Eddowesselink/PhD.git
 ```
-2. Navigate to the code/Thresholding directory where you stored the repository
+2. Navigate to the code directory where you stored the repository
 
 ```bash
-cd PhD/code/Thresholding
+cd `/path/to/your/repository`
 ```
-3. Run the script main_thresholding.py with the required arguments:
+### Thresholding
+-  Run the script main_thresholding.py with the required arguments:
 ```bash
 python main_thresholding.py --data_dir /path/to/your/data --kmeans --gmm
 ```
 Replace `/path/to/your/data` with the path to the directory containing your MRI data. You can specify either `--kmeans` or `--gmm` to choose between KMeans or Gaussian Mixture Model clustering for segmentation.
 
+### CNN
+- Run the script main_CNN.py with the required arguments:
+```bash
+python main_thresholding.py --data_dir /path/to/your/data --model_dir /path/to/your/data
+```
+Replace `/path/to/your/data` in --data_dir with the path to the directory containing your MRI data. 
+Replace `/path/to/your/data` in --model_dir with the path to the directory containing the model_parameters. 
--- code/CNN/CNN.py
+++ code/CNN/CNN.py
@@ -1,6 +1,8 @@
 import os
 import tempfile
 import glob
+import argparse
+import torch
 from monai.inferers import SliceInferer
 from monai.networks.nets import UNet
 from monai.transforms import (
@@ -19,41 +21,28 @@ from monai.transforms import (
     KeepLargestConnectedComponentd,
 )
 from monai.networks.layers import Norm
-from monai.utils import  set_determinism
-from monai.data import (
-    Dataset,
-    DataLoader,
-    decollate_batch,
-)
-import torch
+from monai.utils import set_determinism
+from monai.data import Dataset, DataLoader, decollate_batch
 
-#setup data directory
-directory = os.environ.get("Monai_Directory")
-root_dir = tempfile.mkdtemp() if directory is None else directory
-save_dir = 'D:/PS_Muscle_Segmentation/Monai/Lumbar_spine/CNN'
+def main(data_dir, model_dir):
+    save_dir = os.path.join(args.data_dir, "CNN")
+    images = sorted(glob.glob(os.path.join(args.data_dir, "*img.nii.gz")))
+    # Check if any image files are found
+    if not images:
+        raise ValueError("No MRI images ending with 'img.nii.gz' found in the specified directory.")
+    test_files = [{"image": img} for img in zip(images)]
 
-# load dataset 
-data_dir = 'D:/PS_Muscle_Segmentation/Monai/Lumbar_spine'
-images = sorted(glob.glob(os.path.join(data_dir, "testing", "*F.nii.gz")))
-test_files = [{"image": img} for img in zip(images)]
+    # Set important parameters
+    roi_size = (112,112)
+    spatial_window_batch_size = 1
+    amount_of_labels = 9
+    pix_dim  = (1,1,-1)
 
-spatial_window_size = (256,256,1)
-roi_size = spatial_window_size[0:2]
-spatial_window_batch_size = 4
-batch_size_training = 50
-amount_of_labels = 7
-inference_iteration = 30000
-pix_dim  = (1,1,1)
-model_save_best = f'model_UNET_Lumbar_spine_iteration_{inference_iteration}.pth'
-model_continue_training = f'model_UNET_Lumbar_spine_iteration_{inference_iteration}.pth'
-learning_rate = 1e-4
-weight_decay= 1e-5
+    # Set seed for reproducibility
+    set_determinism(seed=0)
 
-#set seed for reproducibility (identical to training part)
-set_determinism(seed=0)
-
-#create transforms identical to training part, but here we don't specifiy the label key
-validation_original = Compose([
+    # Setup transforms
+    validation_original = Compose([
         LoadImaged(keys=["image"]),
         EnsureChannelFirstd(keys=["image"]),
         Spacingd(
@@ -66,54 +55,61 @@ validation_original = Compose([
         EnsureTyped(keys=["image"])
     ])
 
-#create iterable dataset and dataloader, identical to training part
-validation_original_dataset = Dataset(
-    data=test_files, transform=validation_original,
-)
+    # Create dataset and dataloader
+    validation_original_dataset = Dataset(data=test_files, transform=validation_original)
+    validation_original_loader = DataLoader(validation_original_dataset, batch_size=1, shuffle=False, num_workers=0, pin_memory=True)
 
-validation_original_loader = DataLoader(
-    validation_original_dataset , batch_size=1, shuffle=False, num_workers=0, pin_memory=True)
+    # Device config
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-#device config
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # Setup post transforms
+    post_transforms = Compose([
+        Invertd(
+            keys="pred",
+            transform=validation_original,
+            orig_keys="image",
+            meta_keys="pred_meta_dict",
+            orig_meta_keys="image_meta_dict",
+            meta_key_postfix="meta_dict",
+            nearest_interp=False,
+            to_tensor=True,
+            device=device
+        ),
+        AsDiscreted(keys="pred", argmax=True),
+        FillHolesd(keys="pred", applied_labels=[1,2,3,4,5,6,7,8,9]),
+        KeepLargestConnectedComponentd(keys="pred", applied_labels=[1,2,3,4,5,6,7,8,9]),
+        SaveImaged(keys="pred", meta_keys="pred_meta_dict", output_dir=save_dir, output_dtype=('int16'), separate_folder = False, resample=False)
+    ])
 
-post_transforms = Compose([
-    Invertd(
-        keys="pred",
-        transform=validation_original,
-        orig_keys="image",
-        meta_keys="pred_meta_dict",
-        orig_meta_keys="image_meta_dict",
-        meta_key_postfix="meta_dict",
-        nearest_interp=False,
-        to_tensor=True,
-        device = device
-    ),
-    AsDiscreted(keys="pred", argmax=True),
-    FillHolesd(keys="pred", applied_labels=[1,2,3,4,5,6,7]),
-    KeepLargestConnectedComponentd(keys="pred", applied_labels=[1,2,3,4,5,6,7]),
-    SaveImaged(keys="pred", meta_keys="pred_meta_dict", output_dir=save_dir, output_dtype=('int16'), resample=False, separate_folder=False)
-])
+    # Create model
+    model= UNet(
+        spatial_dims=2,
+        in_channels=1,
+        out_channels=amount_of_labels,
+        channels=(64, 128, 256, 512, 1024),
+        act= 'LeakyRelu',
+        strides=(2, 2, 2, 2),
+        num_res_units=2,
+        norm=Norm.INSTANCE,
+    ).to(device)
 
-model= UNet(
-    spatial_dims=2,
-    in_channels=1,
-    out_channels=amount_of_labels,
-    channels=(16, 32, 64, 128, 256),
-    act= 'LeakyRelu',
-    strides=(2, 2, 2, 2),
-    num_res_units=2,
-    norm=Norm.INSTANCE,
-).to(device)
+    # Load pre-existing model if continuing training
+    model.load_state_dict(torch.load(os.path.join(args.model_dir)))
+    model.eval()
+    
+    # Inference
+    with torch.no_grad():
+        for i, test_data in enumerate(validation_original_loader):
+            val_inputs = test_data["image"].to(device)
+            axial_inferer = SliceInferer(roi_size=roi_size, sw_batch_size=spatial_window_batch_size, spatial_dim=2)
+            test_data["pred"] = axial_inferer(val_inputs, model)
+            val_data = [post_transforms(i) for i in decollate_batch(test_data)]
 
-#load pre-excisting model 
-model.load_state_dict(torch.load(
-    os.path.join(root_dir,'Models',"lumbar_spine_UNET_per_iteration",model_save_best)))
-model.eval()
-
-with torch.no_grad():
-    for i, test_data in enumerate(validation_original_loader):
-        val_inputs = test_data["image"].to(device)
-        axial_inferer = SliceInferer(roi_size=roi_size, sw_batch_size=spatial_window_batch_size, spatial_dim=2)
-        test_data["pred"] = axial_inferer(val_inputs, model)
-        val_data = [post_transforms(i) for i in decollate_batch(test_data)]
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Segment MRI images using a pre-trained model.")
+    parser.add_argument("--data_dir", type=str, help="Path to the data directory")
+    parser.add_argument("--model_dir", type=str, help="Path to the directory containing the model parameters")
+    args = parser.parse_args()
+    main(args.data_dir, args.model_dir)
+    print('There you go!')
+    print('Happy to help!')
